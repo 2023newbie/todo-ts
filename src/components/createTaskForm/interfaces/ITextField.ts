70f05b3e8791346b3620d83cc9c481b63b0af107
Obar <ducwebdev@gmail.com>
2024-04-18 21:35:27 +0700

    first commit

---
+++ src/components/createTaskForm/interfaces/ITextField.ts
@@ -0,0 +1,10 @@
+import { ChangeEvent } from 'react';
+import { IDisabled } from './IDisabled';
+
+export interface ITextField extends IDisabled {
+    onChange?: (
+        e: ChangeEvent<
+            HTMLInputElement | HTMLTextAreaElement
+        >,
+    ) => void;
+}
